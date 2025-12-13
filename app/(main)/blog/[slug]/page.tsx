b2ef7397b77a007ade5354267fbf23689ede9b1d
Bamidele Aminu <obedaminu303@gmail.com>
2025-12-13 21:55:03 +0100

    update made to recent changes

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -54,7 +54,7 @@ export async function generateMetadata({
         {
           url: image,
           width: 1200,
-          height: 630,
+          height: 1200,
           alt: title,
         },
       ],
